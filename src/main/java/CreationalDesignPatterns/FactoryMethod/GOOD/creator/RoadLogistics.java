7a35d4faa961696676c7f46df5f7d1a2f9e5fc40
HarmanGi <harman.giri@kuonitumlare.com>
2026-05-14 13:35:39 +0530

    Refactor RoadLogistics class by making it abstract and splitting its functionality into specific subclasses (TruckLogistics and CarLogistics).

--- src/main/java/CreationalDesignPatterns/FactoryMethod/GOOD/creator/RoadLogistics.java
+++ src/main/java/CreationalDesignPatterns/FactoryMethod/GOOD/creator/RoadLogistics.java
@@ -1,11 +1,4 @@
 package CreationalDesignPatterns.FactoryMethod.GOOD.creator;
 
-import CreationalDesignPatterns.FactoryMethod.GOOD.product.Transport;
-import CreationalDesignPatterns.FactoryMethod.GOOD.product.Truck;
-
-public class RoadLogistics extends Logistics {
-    @Override
-    public Transport createTransport() {
-        return new Truck();
-    }
+public abstract class RoadLogistics extends Logistics {
 }
--- src/main/java/CreationalDesignPatterns/FactoryMethod/GOOD/creator/TruckLogistics.java
+++ src/main/java/CreationalDesignPatterns/FactoryMethod/GOOD/creator/TruckLogistics.java
@@ -1,13 +1,11 @@
 package CreationalDesignPatterns.FactoryMethod.GOOD.creator;
 
-import CreationalDesignPatterns.FactoryMethod.GOOD.product.Car;
 import CreationalDesignPatterns.FactoryMethod.GOOD.product.Transport;
 import CreationalDesignPatterns.FactoryMethod.GOOD.product.Truck;
 
-public class CarLogistics extends RoadLogistics {
-    
+public class TruckLogistics extends RoadLogistics{
     @Override
     public Transport createTransport() {
-        return new Car();
+        return new Truck();
     }
 }
